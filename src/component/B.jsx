805e8720279d6f6b42b48a396d3b9613a067cde4
Archana_Vajpara <arvajpara1806@gmail.com>
2025-09-01 10:56:52 +0530

    Add files via upload

---
+++ src/component/B.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import C from './C'
+
+function B(props){
+  return (
+    <div>
+      <C msg={props.msg}/>
+    </div>
+  )
+}
+export default B
+
---
+++ src/component/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Navbar() {
+
+    return(
+    <>
+        <nav class="Navbar">
+        <div class="container-fluid">
+            <ul class="navbar">
+                    <li class="nav-item"><a class="nav-link text-light" href="#">Home </a></li>
+                    <li class="nav-item"><a class="nav-link text-light" href="#">About</a></li>
+                    <li class="nav-item"><a class="nav-link text-light" href="#">Profile</a></li>
+                    <li class="nav-item"><a class="nav-link text-light" href="#">Contact Us</a></li>
+                </ul>
+            
+        </div>
+        </nav>
+    </>
+    );
+}
+
